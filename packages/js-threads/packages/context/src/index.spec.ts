06c9cd7ee8923b96b7a15720e2fc66fc2d64c52a
Andrew W. Hill <andrew@textile.io>
2020-05-27 22:17:04 -0700

    Merge pull request #238 from textileio/andrew/adding-context-tests
    feat(tests): adds context tests to avoid regressions

--- packages/js-threads/packages/context/src/index.spec.ts
+++ packages/js-threads/packages/context/src/index.spec.ts
@@ -2,6 +2,8 @@ import { expect } from 'chai'
 import { Context, ContextInterface, expirationError } from './index'
 
 describe('Context', () => {
+  let validJson = {}
+  let validMsg = ''
   it('should throw an exception when working with an expired msg', async () => {
     const context: ContextInterface = new Context()
     try {
@@ -15,12 +17,47 @@ describe('Context', () => {
     } catch (err) {
       expect(err).to.equal(expirationError)
     }
-    const msg = new Date(Date.now() + 1000 * 60).toUTCString()
+    validMsg = new Date(Date.now() + 1000 * 60).toUTCString()
     context.withAPISig({
       sig: 'fake',
+      msg: validMsg,
+    })
+    validJson = context.toJSON()
+    expect(validJson).to.haveOwnProperty('x-textile-api-sig-msg', validMsg)
+  })
+  it('should not throw when creating fromUserAuth', async () => {
+    const msg = new Date(Date.now() + 1000 * 60).toUTCString()
+    const context = Context.fromUserAuth({
+      sig: 'fake',
       msg,
+      token: 'fake',
+      key: 'fake',
     })
     const json = context.toJSON()
     expect(json).to.haveOwnProperty('x-textile-api-sig-msg', msg)
+  })
+  it('should not throw when creating from valid JSON', async () => {
+    const context = Context.fromJSON(validJson)
+    const json = context.toJSON()
+    expect(json).to.haveOwnProperty('x-textile-api-sig-msg', validMsg)
+  })
+  it('should throw when creating from invalid JSON', async () => {
+    const context: ContextInterface = new Context()
+    context.withAPISig({
+      sig: 'fake',
+      // Create msg that will be valid for 0.5 second
+      msg: new Date(Date.now() + 500).toUTCString(),
+    })
+    const json = context.toJSON()
+    const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+    await sleep(600)
+
+    try {
+      const context = Context.fromJSON(json)
+      context.toJSON()
+      throw new Error('should have thrown')
+    } catch (err) {
+      expect(err).to.equal(expirationError)
+    }
   })
 })
