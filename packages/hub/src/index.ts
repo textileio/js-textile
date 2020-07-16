2785ed0162784af70d49676c70b75989565ccfca
Carson Farmer <carson.farmer@gmail.com>
2020-07-16 16:25:12 -0700

    fix: disale prettier import/export defaults
    Signed-off-by: Carson Farmer <carson.farmer@gmail.com>

--- packages/hub/src/index.ts
+++ packages/hub/src/index.ts
@@ -1,12 +1,8 @@
+/* eslint-disable prettier/prettier */
 /**
  * A web-gRPC wrapper client for communicating with the web-gRPC enabled Textile ThreadDB & Buckets APIs.
  * @packageDocumentation
  */
-
-export * from "@textile/buckets"
-export { Client } from "@textile/hub-threads-client"
-export * from "@textile/security"
-export { Identity } from "@textile/threads-core"
 export {
   Collection,
   Config,
@@ -22,4 +18,8 @@ export {
   ReadonlyCollection,
   StartOptions,
 } from "@textile/threads-database"
+export * from "@textile/buckets"
+export { Client } from "@textile/hub-threads-client"
+export * from "@textile/security"
+export { Identity } from "@textile/threads-core"
 export { ThreadID, Variant } from "@textile/threads-id"
