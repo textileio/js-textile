36a6e07aa9b166be542bc21fe7cfa6e6babb4b04
Andrew W. Hill <andrew@textile.io>
2020-07-30 09:32:08 -0700

    chore(api): remove database from hub exported classes and docs (#379)
    Signed-off-by: Andrew Hill <andrew@textile.io>

--- packages/hub/src/index.ts
+++ packages/hub/src/index.ts
@@ -2,22 +2,6 @@
  * A web-gRPC wrapper client for communicating with the web-gRPC enabled Textile ThreadDB & Buckets APIs.
  * @packageDocumentation
  */
-
-export {
-  mismatchError,
-  missingIdentity,
-  existingKeyError,
-  DatabaseSettings,
-  StartOptions,
-  Database,
-  FilterQuery,
-  JSONSchema,
-  Config,
-  Options,
-  Document,
-  Collection,
-  ReadonlyCollection,
-} from '@textile/threads-database'
 export { ThreadID, Variant } from '@textile/threads-id'
 export { Identity } from '@textile/threads-core'
 export { Client } from '@textile/hub-threads-client'
