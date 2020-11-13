ac41b7b97e317ad37d36a66eee583c8f6ebb9eb6
Aaron Sutula <hi@asutula.com>
2020-11-12 21:04:17 -0600

    chore: update to powergate 1.1.2 and update hub
    Signed-off-by: Aaron Sutula <hi@asutula.com>

--- packages/pow/src/api/index.ts
+++ packages/pow/src/api/index.ts
@@ -1,38 +1,19 @@
 import { ContextInterface } from '@textile/context'
 import { GrpcConnection } from '@textile/grpc-connection'
 import {
-  AddrsRequest,
-  AddrsResponse,
-  InfoRequest,
-  InfoResponse,
-  ListDealRecordsConfig,
-  ListRetrievalDealRecordsRequest,
-  ListRetrievalDealRecordsResponse,
-  ListStorageDealRecordsRequest,
-  ListStorageDealRecordsResponse,
-  NewAddrRequest,
-  NewAddrResponse,
-  SendFilRequest,
-  SendFilResponse,
-  ShowAllRequest,
-  ShowAllResponse,
-  ShowRequest,
-  ShowResponse,
-} from '@textile/grpc-powergate-client/dist/ffs/rpc/rpc_pb'
-import { RPCService as FFSRPCService } from '@textile/grpc-powergate-client/dist/ffs/rpc/rpc_pb_service'
-import { CheckRequest, CheckResponse } from '@textile/grpc-powergate-client/dist/health/rpc/rpc_pb'
-import { RPCService as HealthRPCService } from '@textile/grpc-powergate-client/dist/health/rpc/rpc_pb_service'
-import {
-  ConnectednessRequest,
-  ConnectednessResponse,
-  FindPeerRequest,
-  FindPeerResponse,
-  PeersRequest,
-  PeersResponse,
-} from '@textile/grpc-powergate-client/dist/net/rpc/rpc_pb'
-import { RPCService as NetRPCService } from '@textile/grpc-powergate-client/dist/net/rpc/rpc_pb_service'
-import { BalanceRequest, BalanceResponse } from '@textile/grpc-powergate-client/dist/wallet/rpc/rpc_pb'
-import { RPCService as WalletRPCService } from '@textile/grpc-powergate-client/dist/wallet/rpc/rpc_pb_service'
+  AddressesRequest,
+  AddressesResponse,
+  BalanceRequest,
+  BalanceResponse,
+  CidInfoRequest,
+  CidInfoResponse,
+  DealRecordsConfig,
+  RetrievalDealRecordsRequest,
+  RetrievalDealRecordsResponse,
+  StorageDealRecordsRequest,
+  StorageDealRecordsResponse,
+} from '@textile/grpc-powergate-client/dist/powergate/user/v1/user_pb'
+import { UserService } from '@textile/grpc-powergate-client/dist/powergate/user/v1/user_pb_service'
 import log from 'loglevel'
 
 const logger = log.getLogger('pow-api')
@@ -40,146 +21,9 @@ const logger = log.getLogger('pow-api')
 /**
  * @internal
  */
-export async function health(api: GrpcConnection, ctx?: ContextInterface): Promise<CheckResponse.AsObject> {
-  logger.debug('health request')
-  const res: CheckResponse = await api.unary(HealthRPCService.Check, new CheckRequest(), ctx)
-  return res.toObject()
-}
-
-/**
- * @internal
- */
-export async function peers(api: GrpcConnection, ctx?: ContextInterface): Promise<PeersResponse.AsObject> {
-  logger.debug('peers request')
-  const res: PeersResponse = await api.unary(NetRPCService.Peers, new PeersRequest(), ctx)
-  return res.toObject()
-}
-
-/**
- * @internal
- */
-export async function findPeer(
-  api: GrpcConnection,
-  peerId: string,
-  ctx?: ContextInterface,
-): Promise<FindPeerResponse.AsObject> {
-  logger.debug('find peer request')
-  const req = new FindPeerRequest()
-  req.setPeerId(peerId)
-  const res: FindPeerResponse = await api.unary(NetRPCService.FindPeer, req, ctx)
-  return res.toObject()
-}
-
-/**
- * @internal
- */
-export async function connectedness(
-  api: GrpcConnection,
-  peerId: string,
-  ctx?: ContextInterface,
-): Promise<ConnectednessResponse.AsObject> {
-  logger.debug('find peer request')
-  const req = new ConnectednessRequest()
-  req.setPeerId(peerId)
-  const res: ConnectednessResponse = await api.unary(NetRPCService.Connectedness, req, ctx)
-  return res.toObject()
-}
-
-/**
- * @internal
- */
-export async function addrs(api: GrpcConnection, ctx?: ContextInterface): Promise<AddrsResponse.AsObject> {
-  logger.debug('addrs request')
-  const res: AddrsResponse = await api.unary(FFSRPCService.Addrs, new AddrsRequest(), ctx)
-  return res.toObject()
-}
-
-/**
- * @internal
- */
-export async function newAddr(
-  api: GrpcConnection,
-  name: string,
-  type: 'bls' | 'secp256k1',
-  makeDefault: boolean,
-  ctx?: ContextInterface,
-): Promise<NewAddrResponse.AsObject> {
-  logger.debug('newAddr request')
-  const req = new NewAddrRequest()
-  req.setName(name)
-  req.setAddressType(type)
-  req.setMakeDefault(makeDefault)
-  const res: NewAddrResponse = await api.unary(FFSRPCService.NewAddr, req, ctx)
-  return res.toObject()
-}
-
-/**
- * @internal
- */
-export async function info(api: GrpcConnection, ctx?: ContextInterface): Promise<InfoResponse.AsObject> {
-  const res: InfoResponse = await api.unary(FFSRPCService.Info, new InfoRequest(), ctx)
-  return res.toObject()
-}
-
-/**
- * @internal
- */
-export async function show(
-  api: GrpcConnection,
-  cid: string,
-  ctx?: ContextInterface,
-): Promise<SendFilResponse.AsObject> {
-  const req = new ShowRequest()
-  req.setCid(cid)
-  const res: ShowResponse = await api.unary(FFSRPCService.Show, req, ctx)
-  return res.toObject()
-}
-
-/**
- * @internal
- */
-export async function showAll(api: GrpcConnection, ctx?: ContextInterface): Promise<ShowAllResponse.AsObject> {
-  const res: ShowAllResponse = await api.unary(FFSRPCService.ShowAll, new ShowAllRequest(), ctx)
-  return res.toObject()
-}
-
-/**
- * @internal
- */
-export async function listStorageDealRecords(
-  api: GrpcConnection,
-  config: ListDealRecordsConfig.AsObject,
-  ctx?: ContextInterface,
-): Promise<ListStorageDealRecordsResponse.AsObject> {
-  const c = new ListDealRecordsConfig()
-  c.setAscending(config.ascending)
-  c.setDataCidsList(config.dataCidsList)
-  c.setFromAddrsList(config.fromAddrsList)
-  c.setIncludeFinal(config.includeFinal)
-  c.setIncludePending(config.includePending)
-  const req = new ListStorageDealRecordsRequest()
-  req.setConfig(c)
-  const res: ListStorageDealRecordsResponse = await api.unary(FFSRPCService.ListStorageDealRecords, req, ctx)
-  return res.toObject()
-}
-
-/**
- * @internal
- */
-export async function listRetrievalDealRecords(
-  api: GrpcConnection,
-  config: ListDealRecordsConfig.AsObject,
-  ctx?: ContextInterface,
-): Promise<ListRetrievalDealRecordsResponse.AsObject> {
-  const c = new ListDealRecordsConfig()
-  c.setAscending(config.ascending)
-  c.setDataCidsList(config.dataCidsList)
-  c.setFromAddrsList(config.fromAddrsList)
-  c.setIncludeFinal(config.includeFinal)
-  c.setIncludePending(config.includePending)
-  const req = new ListRetrievalDealRecordsRequest()
-  req.setConfig(c)
-  const res: ListRetrievalDealRecordsResponse = await api.unary(FFSRPCService.ListRetrievalDealRecords, req, ctx)
+export async function addresses(api: GrpcConnection, ctx?: ContextInterface): Promise<AddressesResponse.AsObject> {
+  logger.debug('addresses request')
+  const res: AddressesResponse = await api.unary(UserService.Addresses, new AddressesRequest(), ctx)
   return res.toObject()
 }
 
@@ -193,6 +37,60 @@ export async function balance(
 ): Promise<BalanceResponse.AsObject> {
   const req = new BalanceRequest()
   req.setAddress(address)
-  const res: BalanceResponse = await api.unary(WalletRPCService.Balance, req, ctx)
+  const res: BalanceResponse = await api.unary(UserService.Balance, req, ctx)
+  return res.toObject()
+}
+
+/**
+ * @internal
+ */
+export async function cidInfo(
+  api: GrpcConnection,
+  ctx?: ContextInterface,
+  ...cids: string[]
+): Promise<CidInfoResponse.AsObject> {
+  const req = new CidInfoRequest()
+  req.setCidsList(cids)
+  const res: CidInfoResponse = await api.unary(UserService.CidInfo, req, ctx)
+  return res.toObject()
+}
+
+/**
+ * @internal
+ */
+export async function storageDealRecords(
+  api: GrpcConnection,
+  config: DealRecordsConfig.AsObject,
+  ctx?: ContextInterface,
+): Promise<StorageDealRecordsResponse.AsObject> {
+  const c = new DealRecordsConfig()
+  c.setAscending(config.ascending)
+  c.setDataCidsList(config.dataCidsList)
+  c.setFromAddrsList(config.fromAddrsList)
+  c.setIncludeFinal(config.includeFinal)
+  c.setIncludePending(config.includePending)
+  const req = new StorageDealRecordsRequest()
+  req.setConfig(c)
+  const res: StorageDealRecordsResponse = await api.unary(UserService.StorageDealRecords, req, ctx)
+  return res.toObject()
+}
+
+/**
+ * @internal
+ */
+export async function retrievalDealRecords(
+  api: GrpcConnection,
+  config: DealRecordsConfig.AsObject,
+  ctx?: ContextInterface,
+): Promise<RetrievalDealRecordsResponse.AsObject> {
+  const c = new DealRecordsConfig()
+  c.setAscending(config.ascending)
+  c.setDataCidsList(config.dataCidsList)
+  c.setFromAddrsList(config.fromAddrsList)
+  c.setIncludeFinal(config.includeFinal)
+  c.setIncludePending(config.includePending)
+  const req = new RetrievalDealRecordsRequest()
+  req.setConfig(c)
+  const res: RetrievalDealRecordsResponse = await api.unary(UserService.RetrievalDealRecords, req, ctx)
   return res.toObject()
 }
--- packages/pow/src/pow.ts
+++ packages/pow/src/pow.ts
@@ -6,38 +6,16 @@ import {
   WithUserAuthOptions,
 } from '@textile/grpc-authentication'
 import {
-  AddrsResponse,
-  InfoResponse,
-  ListDealRecordsConfig,
-  ListRetrievalDealRecordsResponse,
-  ListStorageDealRecordsResponse,
-  NewAddrResponse,
-  SendFilResponse,
-  ShowAllResponse,
-} from '@textile/grpc-powergate-client/dist/ffs/rpc/rpc_pb'
-import { CheckResponse } from '@textile/grpc-powergate-client/dist/health/rpc/rpc_pb'
-import {
-  ConnectednessResponse,
-  FindPeerResponse,
-  PeersResponse,
-} from '@textile/grpc-powergate-client/dist/net/rpc/rpc_pb'
-import { BalanceResponse } from '@textile/grpc-powergate-client/dist/wallet/rpc/rpc_pb'
+  AddressesResponse,
+  BalanceResponse,
+  CidInfoResponse,
+  DealRecordsConfig,
+  RetrievalDealRecordsResponse,
+  StorageDealRecordsResponse,
+} from '@textile/grpc-powergate-client/dist/powergate/user/v1/user_pb'
 import { KeyInfo, UserAuth } from '@textile/security'
 import log from 'loglevel'
-import {
-  addrs,
-  balance,
-  connectedness,
-  findPeer,
-  health,
-  info,
-  listRetrievalDealRecords,
-  listStorageDealRecords,
-  newAddr,
-  peers,
-  show,
-  showAll,
-} from './api'
+import { addresses, balance, cidInfo, retrievalDealRecords, storageDealRecords } from './api'
 
 const logger = log.getLogger('pow')
 /**
@@ -169,112 +147,11 @@ export class Pow extends GrpcAuthentication {
   }
 
   /**
-   * @beta
-   */
-  async health(): Promise<CheckResponse.AsObject> {
-    return health(this)
-  }
-
-  /**
-   * Gets the Powergate node Filecoin peers. This method
-   * requires a valid user, token, and session.
-   * @beta
-   *
-   * @example
-   * ```typescript
-   * import { Pow } from "@textile/hub"
-   *
-   * async function example(pow: Pow) {
-   *    const peersResp = await pow.peers()
-   * }
-   * ```
-   */
-  async peers(): Promise<PeersResponse.AsObject> {
-    return peers(this)
-  }
-
-  /**
-   * Find a peer by peer id.
-   * @beta
-   * @param peerId The peer id of the peer you want to find.
-   */
-  async findPeer(peerId: string): Promise<FindPeerResponse.AsObject> {
-    return findPeer(this, peerId)
-  }
-
-  /**
-   * Query for network connectedness to the specified peer.
-   * @beta
-   * @param peerId The peer id to test connectedness for.
-   */
-  async connectedness(peerId: string): Promise<ConnectednessResponse.AsObject> {
-    return connectedness(this, peerId)
-  }
-
-  /**
    * List all Filecoin wallet addresses associated with the current account or user.
    * @beta
    */
-  async addrs(): Promise<AddrsResponse.AsObject> {
-    return addrs(this)
-  }
-
-  /**
-   * Create a new Filecoin wallet address.
-   * @beta
-   * @param name A human readable name for the wallet address.
-   * @param type The type of address to create, wither bls or secp256k1.
-   * @param makeDefault Whether or not to make the new address the default address to use in Filecoin transactions.
-   */
-  async newAddr(name: string, type: 'bls' | 'secp256k1', makeDefault: boolean): Promise<NewAddrResponse.AsObject> {
-    return newAddr(this, name, type, makeDefault)
-  }
-
-  /**
-   * Query for general information about Powergate associated with the current account/user.
-   * @beta
-   */
-  async info(): Promise<InfoResponse.AsObject> {
-    return info(this)
-  }
-
-  /**
-   * Show information about data stored in Powergate.
-   * @beta
-   * @param cid The cid of the data to show information about.
-   */
-  async show(cid: string): Promise<SendFilResponse.AsObject> {
-    return show(this, cid)
-  }
-
-  /**
-   * Show information for all data stored in Powergate for the current account/user.
-   * @beta
-   */
-  async showAll(): Promise<ShowAllResponse.AsObject> {
-    return showAll(this)
-  }
-
-  /**
-   * Query for Filecoin storage deal records for the current account/user.
-   * @beta
-   * @param config A config object to control the behavior of the query.
-   */
-  async listStorageDealRecords(
-    config: ListDealRecordsConfig.AsObject,
-  ): Promise<ListStorageDealRecordsResponse.AsObject> {
-    return listStorageDealRecords(this, config)
-  }
-
-  /**
-   * Query for Filecoin retrieval deal records for the current account/user.
-   * @beta
-   * @param config A config object to control the behavior of the query.
-   */
-  async listRetrievalDealRecords(
-    config: ListDealRecordsConfig.AsObject,
-  ): Promise<ListRetrievalDealRecordsResponse.AsObject> {
-    return listRetrievalDealRecords(this, config)
+  async addresses(): Promise<AddressesResponse.AsObject> {
+    return addresses(this)
   }
 
   /**
@@ -284,5 +161,32 @@ export class Pow extends GrpcAuthentication {
    */
   async balance(address: string): Promise<BalanceResponse.AsObject> {
     return balance(this, address)
+  }
+
+  /**
+   * Get information about the storage and job job state of cids stored by the user.
+   * @beta
+   * @param cids The cids to get info for.
+   */
+  async cidInfo(...cids: string[]): Promise<CidInfoResponse.AsObject> {
+    return cidInfo(this, undefined, ...cids)
+  }
+
+  /**
+   * Query for Filecoin storage deal records for the current account/user.
+   * @beta
+   * @param config A config object to control the behavior of the query.
+   */
+  async storageDealRecords(config: DealRecordsConfig.AsObject): Promise<StorageDealRecordsResponse.AsObject> {
+    return storageDealRecords(this, config)
+  }
+
+  /**
+   * Query for Filecoin retrieval deal records for the current account/user.
+   * @beta
+   * @param config A config object to control the behavior of the query.
+   */
+  async retrievalDealRecords(config: DealRecordsConfig.AsObject): Promise<RetrievalDealRecordsResponse.AsObject> {
+    return retrievalDealRecords(this, config)
   }
 }
