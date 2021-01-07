cb2cd724813ca2cb65af6e22a1fd11766049782c
Carson Farmer <carson.farmer@gmail.com>
2021-01-07 14:46:00 -0800

    chore: delete old structure
    Signed-off-by: Carson Farmer <carson.farmer@gmail.com>

--- packages/js-threads/packages/multiaddr/README.md
+++
@@ -1,3 +0,0 @@
-# Multiaddress _(multiaddr)_
-
-**WARN** This is not a supported developer library. Do not use as a direct dependency in your projects.
--- packages/js-threads/packages/threads/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "@textile/security"
-// eslint-disable-next-line prettier/prettier
-export * from "@textile/threads-id"
-// eslint-disable-next-line prettier/prettier
-export * from "@textile/threads-client"
-export { Identity, PrivateKey } from "@textile/crypto"
